4041d9fb228199094894d851c1177f8e2a296dc4
ya <243678@ids.upchiapas.edu.mx>
2025-10-13 09:59:29 -0600

    se pone el registro que jale

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { Cloud } from "../components/cloud/cloud";
-import { RouterLink } from "@angular/router";
+import { FormGroup, FormControl, FormArray, NgForm, FormsModule } from '@angular/forms';
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-register',
-  imports: [Cloud, RouterLink],
+  imports: [Cloud, RouterLink, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+  constructor(private router: Router) {};
 
+  submit(form: NgForm){
+    console.log(form.value)
+  }
+
+  onSubmit(){
+    console.log("hola")
+    this.router.navigate(['home']);
+  }
 }
