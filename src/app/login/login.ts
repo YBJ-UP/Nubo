5b0d2fbe62e674652c192a78c13e143b90d7b5ce
ya <243678@ids.upchiapas.edu.mx>
2025-10-16 09:19:28 -0600

    cosa

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -3,10 +3,11 @@ import { Cloud } from "../components/cloud/cloud";
 import { RouterLink, Router } from "@angular/router";
 import { FormsModule, NgForm } from '@angular/forms';
 import { LoginType } from "../components/login-type/login-type";
+import { Nube } from "../components/nube/nube";
 
 @Component({
   selector: 'app-login',
-  imports: [Cloud, RouterLink, FormsModule, LoginType],
+  imports: [Cloud, RouterLink, FormsModule, LoginType, Nube],
   templateUrl: './login.html',
   styleUrl: '../register/register.css'
 })
--- src/app/login/students/students.ts
+++ src/app/login/students/students.ts
@@ -3,10 +3,11 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { LoginType } from "../../components/login-type/login-type";
 import { Cloud } from '../../components/cloud/cloud';
 import { Router } from '@angular/router';
+import { Nube } from "../../components/nube/nube";
 
 @Component({
   selector: 'app-students',
-  imports: [LoginType, Cloud, FormsModule],
+  imports: [LoginType, Cloud, FormsModule, Nube],
   templateUrl: './students.html',
   styleUrl: '../../register/register.css'
 })
