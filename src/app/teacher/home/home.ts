b49fbf267fc876a238fec331ff28ff1fd771501e
Yael <243678@ids.upchiapas.edu.mx>
2025-10-18 22:14:28 -0600

    se cambian el color de fondo y se añade el saludo

--- src/app/teacher/home/home.ts
+++ src/app/teacher/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../../components/sidebar/sidebar";
+import { Greeting } from "../../components/greeting/greeting";
 
 @Component({
   selector: 'app-home',
-  imports: [Sidebar],
+  imports: [Sidebar, Greeting],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
