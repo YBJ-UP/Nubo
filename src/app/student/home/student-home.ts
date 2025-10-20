861d06e51ff28f58746e30816a3aea5e60a81671
Yael <243678@ids.upchiapas.edu.mx>
2025-10-20 10:40:31 -0600

    se elimina la barra lateral

--- src/app/student/home/student-home.ts
+++ src/app/student/home/student-home.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Sidebar } from "../../components/sidebar/sidebar";
 import { Greeting } from "../../components/greeting/greeting";
 import { CardsHome } from "../../components/cards-home/cards-home";
+import { Header } from "../../components/header/header";
 
 @Component({
  selector: 'app-student-home',
   standalone: true,
-  imports: [Sidebar, Greeting, CardsHome],
+  imports: [Greeting, CardsHome, Header],
   templateUrl: './student-home.html',
   styleUrls: ['./student-home.css']
 })
