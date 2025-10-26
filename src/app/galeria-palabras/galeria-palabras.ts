243f5a9fa81e6b440e1f51cc183915ee6b8a86c0
Silverz34 <pradogordillomichael@gmail.com>
2025-10-26 17:30:06 -0600

    depur

--- src/app/galeria-palabras/galeria-palabras.ts
+++ src/app/galeria-palabras/galeria-palabras.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../components/sidebar/sidebar";
 import { RouterModule } from '@angular/router';
+import { Header } from "../components/header/header";
 
 @Component({
   selector: 'app-galeria-palabras',
-  imports: [Sidebar],
+  imports: [Sidebar, Header],
   templateUrl: './galeria-palabras.html',
   styleUrl: './galeria-palabras.css'
 })
