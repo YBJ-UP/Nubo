627d3d751ef6d32d1080e7bd2d8ea7e331390219
Silverz34 <pradogordillomichael@gmail.com>
2025-10-26 20:51:09 -0600

    cards-module1

---
+++ src/app/components/cards-palabras/cards-palabras.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-palabras',
+  imports: [],
+  templateUrl: './cards-palabras.html',
+  styleUrl: './cards-palabras.css'
+})
+export class CardsPalabras {
+
+}
--- src/app/student/galeria-palabras/galeria-palabras.ts
+++ src/app/student/galeria-palabras/galeria-palabras.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { Sidebar } from "../components/sidebar/sidebar";
 import { RouterModule } from '@angular/router';
-import { Header } from "../components/header/header";
+import { Header } from '../../components/header/header';
+import { CardsPalabras } from "../../components/cards-palabras/cards-palabras";
 
 @Component({
   selector: 'app-galeria-palabras',
-  imports: [Header],
+  imports: [Header,CardsPalabras],
   templateUrl: './galeria-palabras.html',
   styleUrl: './galeria-palabras.css'
 })
