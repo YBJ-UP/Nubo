62af1f804f3a3261aad64bb41a22821e8ed2d96e
Yael <243678@ids.upchiapas.edu.mx>
2025-10-31 14:35:18 -0600

    se le añade router outlet

--- src/app/student/home/student-home.ts
+++ src/app/student/home/student-home.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Greeting } from "../../components/greeting/greeting";
-import { CardsHome } from "../../components/cards-home/cards-home";
 import { Header } from "../../components/header/header";
+import { RouterModule } from "@angular/router";
 
 @Component({
  selector: 'app-student-home',
   standalone: true,
-  imports: [Greeting, CardsHome, Header],
+  imports: [Greeting, Header, RouterModule],
   templateUrl: './student-home.html',
   styleUrls: ['./student-home.css']
 })
