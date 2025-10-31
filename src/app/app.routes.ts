46e2d9ef14566d51ad2cf1b53ef6121028755132
Yael <243678@ids.upchiapas.edu.mx>
2025-10-31 14:11:28 -0600

    se añaden las rutas de profesor y estudiante

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,61 +2,9 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
 import { Home } from './teacher/home/home';
+import { CardsHome } from './components/cards-home/cards-home';
 
 export const routes: Routes = [
-    /*{
-        path:'',
-        redirectTo:'register',
-        pathMatch:'full'
-    },
-    {
-        path:'register',
-        loadComponent: () => {
-            return import('./register/register').then((m) => m.Register)
-        }
-    },
-    {
-        path:'login/teacher',
-        loadComponent: () => {
-            return import('./login/login').then((m) => m.Login)
-        }
-    },
-    {
-        path:'login/student',
-        loadComponent: () => {
-            return import('./login/students/students').then((m) => m.Students)
-        }
-    },
-    {
-        path:'home/teacher',
-        loadComponent: () => {
-            return import('./teacher/home/home').then((m) => m.Home)
-        }
-    },
-    {
-        path:'home/student',
-        loadComponent: () => {
-            return import('./student/home/student-home').then((m) => m.StudentHome)
-        }
-    },
-    {
-        path:'home/teacher/students',
-        loadComponent: () => {
-            return import('./teacher/create-student/create-student').then((m) => m.CreateStudent)
-        }
-    },
-    {
-        path:'cognitive-abilities',
-        loadComponent: () => {
-            return import('./student/galeria-palabras/galeria-palabras').then((m) => m.GaleriaPalabras)
-        }
-    },
-    {
-        path:'actividad/:id',
-        loadComponent:()=>{
-            return import('./student/actividad-palabras/actividad-palabras').then((m)=> m.ActividadPalabras)
-        }
-    }*/
    {
     path: '',
     redirectTo: 'login',
@@ -69,5 +17,18 @@ export const routes: Routes = [
    {
     path: 'register',
     component: Register
+   },
+   {
+    path: 'teacher',
+    component: Home,
+    children: [
+        { path: '', component: CardsHome }
+    ]
+   },
+   {
+    path: 'student',
+    children: [
+        { path: 'student', component: CardsHome }
+    ]
    }
 ];
