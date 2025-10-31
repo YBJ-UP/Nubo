09f0cc038733b3c3c86c70ec72ea9790553ee5bf
Yael <243678@ids.upchiapas.edu.mx>
2025-10-31 14:45:40 -0600

    cambios de legibilidad e importaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
+
 import { Home } from './teacher/home/home';
-import { StudentHome } from './student/home/student-home';
-import { CardsHome } from './components/cards-home/cards-home';
 import { CreateStudent } from './teacher/create-student/create-student';
+
+import { StudentHome } from './student/home/student-home';
+import { ActividadPalabras } from './student/actividad-palabras/actividad-palabras';
+import { GaleriaPalabras } from './student/galeria-palabras/galeria-palabras';
+
+
+import { CardsHome } from './components/cards-home/cards-home';
+import { CardsPalabras } from './components/cards-palabras/cards-palabras';
 
 export const routes: Routes = [
    {
