768c1c8687638e193f3b5977401949c5fd00b74c
Yael <243678@ids.upchiapas.edu.mx>
2025-10-31 14:33:49 -0600

    se añaden rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,9 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
 import { Home } from './teacher/home/home';
+import { StudentHome } from './student/home/student-home';
 import { CardsHome } from './components/cards-home/cards-home';
+import { CreateStudent } from './teacher/create-student/create-student';
 
 export const routes: Routes = [
    {
@@ -22,13 +24,15 @@ export const routes: Routes = [
     path: 'teacher',
     component: Home,
     children: [
-        { path: '', component: CardsHome }
+        { path: '', component: CardsHome },
+        { path: 'students', component: CreateStudent }
     ]
    },
    {
     path: 'student',
+    component: StudentHome,
     children: [
-        { path: 'student', component: CardsHome }
+        { path: '', component: CardsHome }
     ]
    }
 ];
