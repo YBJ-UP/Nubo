43e78bdf997e1088d1799cb0dd8a0292fb5284f3
Yael <243678@ids.upchiapas.edu.mx>
2025-10-31 14:36:29 -0600

    se convierte en componente de verdad

--- src/app/teacher/create-student/create-student.ts
+++ src/app/teacher/create-student/create-student.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { Sidebar } from "../../components/sidebar/sidebar";
-import { Greeting } from "../../components/greeting/greeting";
-import { Header } from "../../components/header/header";
 
 @Component({
   selector: 'app-create-student',
-  imports: [Sidebar, Greeting, Header],
+  imports: [],
   templateUrl: './create-student.html',
-  styleUrl: '../home/home.css'
+  styleUrl: './create-student.css'
 })
 export class CreateStudent {
 
