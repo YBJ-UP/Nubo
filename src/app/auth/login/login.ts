effb4f66feb023d8b2c545c30245072f96d6b49b
Yael <243678@ids.upchiapas.edu.mx>
2025-11-01 13:38:37 -0600

    noc q hice

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { Cloud } from "../components/cloud/cloud";
+import { Cloud } from "../../components/cloud/cloud";
 import { RouterLink, Router } from "@angular/router";
 import { FormsModule, NgForm } from '@angular/forms';
-import { Nube } from "../components/nube/nube";
+import { Nube } from "../../components/nube/nube";
 
 @Component({
   selector: 'app-login',
   imports: [Cloud, RouterLink, FormsModule, Nube],
   templateUrl: './login.html',
-  styleUrl: '../auth/register/register.css'
+  styleUrl: '../register/register.css'
 })
 export class Login {
 
