20649c07c836b56bdb0a483366a95d2a4ed505c0
Yael <243678@ids.upchiapas.edu.mx>
2025-11-01 17:14:11 -0600

    importacion innecesaria

--- src/app/student/home/student-home.ts
+++ src/app/student/home/student-home.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { Greeting } from "../../components/greeting/greeting";
 import { Header } from "../../components/header/header";
 import { RouterModule } from "@angular/router";
 
 @Component({
  selector: 'app-student-home',
   standalone: true,
-  imports: [Greeting, Header, RouterModule],
+  imports: [Header, RouterModule],
   templateUrl: './student-home.html',
   styleUrls: ['./student-home.css']
 })
