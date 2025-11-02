97b5cc00027e353a92b8201ce8ff16b3e6561c02
Silverz34 <pradogordillomichael@gmail.com>
2025-11-02 01:00:35 -0600

    rediseño de cards

--- src/app/data/memorama-data.ts
+++ src/app/data/memorama-data.ts
@@ -9,42 +9,42 @@ export interface MemoramaData {
 export const MEMORAMA_DATA_MOCK: MemoramaData[] = [
     { 
       id: 1, 
-      titulo: 'Tema: Animales Salvajes', 
+      titulo: ' Animales Salvajes', 
       colorFondo: '#BDE0FE', 
-      imagenUrl: '/assets/memorama/animales.png', 
+      imagenUrl: '/crds.webp', 
       rondaId: 101 
     },
     { 
       id: 2, 
-      titulo: 'Tema: Frutas y Verduras', 
+      titulo: 'Frutas y Verduras', 
       colorFondo: '#F78C8C', 
-      imagenUrl: '/assets/memorama/frutas.png', 
+      imagenUrl: '/crds.webp', 
       rondaId: 102 
     },
     { 
       id: 3, 
-      titulo: 'Tema: Instrumentos Musicales', 
+      titulo: 'Instrumentos Musicales', 
       colorFondo: '#D4BFFF', 
-      imagenUrl: '/assets/memorama/instrumentos.png', 
+      imagenUrl: '/crds.webp', 
       rondaId: 103 
     },
     { 
       id: 4, 
-      titulo: 'Tema: Transporte', 
+      titulo: 'Transporte', 
       colorFondo: '#FEF9C3',
       imagenUrl: '/assets/memorama/transporte.png', 
       rondaId: 104 
     },
     { 
       id: 5, 
-      titulo: 'Tema: Figuras Geométricas', 
+      titulo: 'Figuras Geométricas', 
       colorFondo: '#D9F7C4', 
       imagenUrl: '/assets/memorama/figuras.png', 
       rondaId: 105 
     },
     { 
       id: 6, 
-      titulo: 'Tema: Profesiones', 
+      titulo: 'Profesiones', 
       colorFondo: '#C3D4FE', 
       imagenUrl: '/assets/memorama/profesiones.png', 
       rondaId: 106 
