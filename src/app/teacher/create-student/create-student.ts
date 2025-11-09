5fe7bf0b42ab2b5dc869ece54126f87bed84e0d2
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-11-08 21:52:15 -0600

    Create-student.ts corregido

--- src/app/teacher/create-student/create-student.ts
+++ src/app/teacher/create-student/create-student.ts
@@ -1,16 +1,53 @@
-import { Component} from '@angular/core';
-import studentData from '../../../../public/placeholderData/studentData.json'
-import { FormsModule, NgForm } from "@angular/forms";
-import { RouterModule } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule, NavigationEnd } from "@angular/router";
 import { Student } from '../../interfaces/student';
+import studentData from '../../../../public/placeholderData/studentData.json';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-student',
-  imports: [FormsModule, RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './create-student.html',
   styleUrl: './create-student.css'
 })
+export class CreateStudent implements OnInit {
+  data: Student[] = [];
+  mostrarVistaHija: boolean = false;
 
-export class CreateStudent {
-  data: Student[] = studentData
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.actualizarVistaHija();
+    });
+  }
+
+  ngOnInit(): void {
+    this.cargarEstudiantes();
+    this.actualizarVistaHija();
+  }
+
+  cargarEstudiantes(): void {
+    const estudiantesGuardados = localStorage.getItem('students');
+    if (estudiantesGuardados) {
+      this.data = JSON.parse(estudiantesGuardados);
+    } else {
+      this.data = [...studentData];
+      localStorage.setItem('students', JSON.stringify(this.data));
+    }
+  }
+
+  actualizarVistaHija(): void {
+    const url = this.router.url;
+    this.mostrarVistaHija = url.includes('/new') || url.includes('/view/');
+  }
+
+  crearNuevoAlumno(): void {
+    this.router.navigate(['/teacher/students/new']);
+  }
+
+  verDetalleAlumno(id: number): void {
+    this.router.navigate(['/teacher/students/view', id]);
+  }
 }
