99eefe986f8a0d266a4beb218cfab7a930eb20f9
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-11-11 23:17:11 -0600

    actualizar student-home.ts

--- src/app/student/home/student-home.ts
+++ src/app/student/home/student-home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from "@angular/router";
+import { Header } from "../../components/header/header";
 
 @Component({
   selector: 'app-student-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, Header],
   templateUrl: './student-home.html',
   styleUrls: ['./student-home.css']
 })
