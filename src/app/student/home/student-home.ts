dbf6922a5a8bf02f6ce2883cbec481d9706e4406
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-11-11 23:00:21 -0600

    quitar el import del header

--- src/app/student/home/student-home.ts
+++ src/app/student/home/student-home.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { Header } from "../../components/header/header";
 import { RouterModule } from "@angular/router";
 
 @Component({
- selector: 'app-student-home',
+  selector: 'app-student-home',
   standalone: true,
-  imports: [Header, RouterModule],
+  imports: [RouterModule],
   templateUrl: './student-home.html',
   styleUrls: ['./student-home.css']
 })
-export class StudentHome {
-
-}
+export class StudentHome { }
