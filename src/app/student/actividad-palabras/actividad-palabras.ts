fcd204e5b50087919d7d5c41efebae8af642c72e
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-11-16 18:16:17 -0600

    quitar componente, pruebita de error solucionada

--- src/app/student/actividad-palabras/actividad-palabras.ts
+++ src/app/student/actividad-palabras/actividad-palabras.ts
@@ -1,23 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Location } from '@angular/common';
-
-@Component({
-  selector: 'app-actividad-palabras',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './actividad-palabras.html',
-  styleUrl: './actividad-palabras.css'
-})
-export class ActividadPalabras {
-  
-  constructor(private location: Location) {}
-
-  regresar(): void {
-    this.location.back();
-  }
-}
 
 export interface Palabra {
   id: number;
