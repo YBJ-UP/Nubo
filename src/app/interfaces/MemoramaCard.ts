412155adec5a24a7284ba620fc615b51a7d9e61f
Silverz34 <pradogordillomichael@gmail.com>
2025-12-01 00:34:23 -0600

    activity y contet interfaz

--- src/app/interfaces/MemoramaCard.ts
+++
@@ -1,9 +0,0 @@
-export interface MemoramaCard {
-  id: number;              
-  pairId: number;          
-  pairKey: string;         
-  theme: string;           
-  isFlipped: boolean;      
-  isMatched: boolean;      
-  contentUrl: string;      
-}
--- src/app/interfaces/memorama.ts
+++
@@ -1,7 +0,0 @@
-export interface MemoramaData {
-  id: number;
-  titulo: string;
-  colorFondo: string; 
-  imagenUrl: string; 
-  rondaId: number;   
-}
