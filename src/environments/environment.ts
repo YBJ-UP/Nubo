049f974e1c047f39c33a6abc8487b4106bb6778f
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-12-01 03:08:10 -0600

    enviroment modificados nuevamente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://127.0.0.1:9000'
+    apiUrl: 'http://localhost:9000'
 
       // Para producción:
     // apiUrl: 'https://tu-api-en-produccion.com'
