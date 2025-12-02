8afd8a81e86f36b9b496aa57558114ac99ff880f
Yael <243678@ids.upchiapas.edu.mx>
2025-12-01 23:12:37 -0600

    Se añade la función de que ponga el nombre del maestro en el encabezado

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,5 +1,7 @@
 import { Component, input } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { TeacherAuthService } from '../../services/authentication/teacher-auth.service';
+import { StudentAuthService } from '../../services/authentication/student-auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +10,18 @@ import { RouterLink } from "@angular/router";
   styleUrl: './header.css'
 })
 export class Header {
-  view = input("Hogar")
-  name = input("Usuario")
-  role = input("Educador")
-  school = input("Escuela")
+
+  view = "Hogar"
+  name = "Usuario"
+  role = "Educador"
+  school = "Escuela"
+
+  constructor(private teacher: TeacherAuthService, private student: StudentAuthService){
+    if (this.teacher.currentTeacher){
+      this.name = this.teacher.currentTeacher.fullname
+      this.role = "Educador"
+      this.school = this.teacher.currentTeacher.escuela
+    }
+  }
+  
 }
